b82c7e56108db21a8ffb982b32d9a28319f40559
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2022-08-25 11:48:16 -0300

    troco do exe

---
+++ Inicializa Backup MOR.bat
@@ -0,0 +1,12 @@
+@echo
+npm install pm2@latest -g
+
+npm install pm2-windows-startup -g
+
+pm2 delete all
+
+pm2 save --force
+
+pm2 start verificaarquivos.exe
+
+pm2 save --force
--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo
-npm install pm2@latest -g
-
-npm install pm2-windows-startup -g
-
-pm2 start verificaarquivos.exe
-
-pm2 save --force

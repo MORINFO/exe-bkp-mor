c30f7fd9f9f4c9ec0f11b5c4df255012297f6345
BrenoNovais <breno.novais@hotmail.com>
2022-09-02 16:59:27 -0300

    update

--- Inicializa Backup MOR.bat
+++ Inicializa Backup MOR.bat
@@ -1,12 +1,2 @@
 @echo
-npm install pm2@latest -g
-
-npm install pm2-windows-startup -g
-
-pm2 delete all
-
-pm2 save --force
-
-pm2 start BKP-MOR.exe
-
-pm2 save --force
+npm install pm2@latest -g && npm install pm2-windows-startup -g && pm2 delete all && pm2 save --force && pm2 start BKP-MOR.exe && pm2 save --force
